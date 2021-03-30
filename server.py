b7ff2a886fcfb68d4475dc9fd8165c918bbab75d
slavabdev <slav.balykov@gmail.com>
2021-03-30 00:54:38 +0000

    Connect to database

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists('env.py'):
     import env
 
@@ -9,10 +12,17 @@ if os.path.exists('env.py'):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    recipes = list(mongo.db.recipes.find())
+    return render_template('home.html', recipes=recipes)
 
 
 if __name__ == '__main__':
